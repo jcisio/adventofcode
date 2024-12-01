c8c9995e4aab8cd5e29d863e17a69f633827041e
Hai-Nam Nguyen <han.nguyen@criteo.com>
2024-12-01 06:12:10 +0100

    Day 01 Part 2

--- 2024/d01.py
+++ 2024/d01.py
@@ -18,7 +18,7 @@ class Problem:
         return sum([abs(x-y) for x, y in zip(lleft, lright)])
 
     def solve2(self):
-        return 0
+        return sum(sum([r for r in self.input['right'] if l==r]) for l in self.input['left'])
 
 
 class Solver:
@@ -37,4 +37,4 @@ class Solver:
 f = open(__file__[:-3] + '.in', 'r')
 solver = Solver(f.read().strip().split('\n'))
 print("Puzzle 1: ", solver.solve())
-#print("Puzzle 2: ", solver.solve(2))
+print("Puzzle 2: ", solver.solve(2))
