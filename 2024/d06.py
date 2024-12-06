f4ed4baff6da8b52a823278f93372120e5b7f3cf
Hai-Nam Nguyen <han.nguyen@criteo.com>
2024-12-06 06:18:25 +0100

    Day 06 Part 1

--- 2024/d06.py
+++ 2024/d06.py
@@ -3,3 +3,52 @@ Advent Of Code
 --- Day 6: Guard Gallivant ---
 https://adventofcode.com/2024/day/6
 """
+from __future__ import annotations
+from collections import defaultdict
+import parse
+
+
+class Problem:
+    def __init__(self, input) -> None:
+        self.input = input
+        self.nr = len(input)
+        self.nc = len(input[0])
+        for r in range(self.nr):
+            for c in range(self.nc):
+                if self.input[r][c] == '^':
+                    self.p = (r, c)
+        self.dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        self.d = 0
+
+    def solve(self):
+        visited = set([self.p])
+        while True:
+            next = (self.p[0] + self.dirs[self.d][0], self.p[1] + self.dirs[self.d][1])
+            if 0 <= next[0] < self.nr and 0 <= next[1] < self.nc:
+                if self.input[next[0]][next[1]] == '#':
+                    self.d = (self.d + 1) % 4
+                else:
+                    self.p = next
+                    visited.add(next)
+                #print(self.d, next, self.input[next[0]][next[1]])
+            else:
+                break
+        return len(visited)
+
+    def solve2(self):
+        return 0
+
+
+class Solver:
+    def __init__(self, input) -> None:
+        self.input = input
+
+    def solve(self, part=1):
+        problem = Problem(self.input)
+        return problem.solve() if part==1 else problem.solve2()
+
+
+f = open(__file__[:-3] + '.test', 'r')
+solver = Solver(f.read().strip().split('\n'))
+print("Puzzle 1: ", solver.solve())
+#print("Puzzle 2: ", solver.solve(2))
