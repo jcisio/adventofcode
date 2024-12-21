77c5fefa613e0867f399c0762e60686eb45ecd63
Hai-Nam Nguyen <han.nguyen@criteo.com>
2024-12-21 16:38:59 +0100

    Day 21 Part 2 incorrect

--- 2024/d21.py
+++ 2024/d21.py
@@ -18,6 +18,7 @@ class Solver:
 ### No change before this ###
 
 import time
+from functools import cache
 
 
 class Problem:
@@ -66,6 +67,10 @@ class Problem:
                     q.append((d+1, keypad[p][k], seq+self.D[k]))
         return max(result, key=self.score)
 
+    @cache
+    def fast_shortest(self, A, B):
+        return self.find_shortest(A, B, self.K2)
+
     def score(self, seq):
         s = sum(1 for i in range(len(seq)-1) if seq[i]==seq[i+1])
         # print(seq, s)
@@ -102,6 +107,20 @@ class Problem:
         # print(seq, len(s3))
         return int(seq[:-1]) * len(s3)
 
+    @cache
+    def fast_sum(self, seq, depth):
+        if depth == 1:
+            return len(self.encode(seq, self.K2))
+        parts = [s + 'A' for s in seq[:-1].split('A')]
+        if len(parts) == 1:
+            return self.fast_sum(self.encode(parts[0], self.K2), depth-1)
+        return sum(self.fast_sum(part, depth) for part in parts)
+
+    def real_complexity(self, seq, depth):
+        seq2 = self.encode(seq, self.K1)
+        s = self.fast_sum(seq2, depth)
+        return int(seq[:-1]) * s
+
     def debug(self, s3):
         s2 = self.decode(s3, self.K2)
         s1 = self.decode(s2, self.K2)
@@ -111,11 +130,11 @@ class Problem:
         # print(self.find_complexity("179A"))
         # print(self.find_shortest("A", "1", self.K1))
         # self.debug("<v<A>>^A<vA<A>>^AAvAA<^A>A<v<A>>^AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A")
-        return sum(self.find_complexity(seq) for seq in self.input)
-        # return 0
+        # return sum(self.find_complexity(seq) for seq in self.input)
+        return sum(self.real_complexity(seq, 2) for seq in self.input)
 
     def solve2(self):
-        return 0
+        return sum(self.real_complexity(seq, 25) for seq in self.input)
 
 in1 = """
 029A
@@ -124,24 +143,34 @@ in1 = """
 456A
 379A
 
-<^<A^^A>>AvvvA
-v<<A>^Av<A>>^A<AA>AvAA^Av<AAA^>A
-v<A<AA>>^AvA<^A>Av<A<A>>^AvAA<^A>Av<<A>>^AAvA^Av<A^>AA<A>Av<A<A>>^AAA<Av>A^A
+v<<A>>^A<A>AvA<^AA>A<vAAA>^A
+<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A
+
+v<<A>>^A<A>AvA<^AA>Av<AAA^>A
+v<A<AA>>^AAv<<A>>^AAv<A^>Av<<A>>^Av<<A>>^AAv<A<A>>^Av<A<A>>^Av<A<A>>^AA
+
+<A^A>^^A
+v<<A>>^A<A>AvA<^AA>A
+v<A<AA>>^AvAA<^A>Av<<A>>^AvA^Av<A^>Av<<A>^A>AAvA^A
 
 
-^<<A^^A>>AvvvA
-<Av<AA>>^A<AA>AvAA^A<vAAA>^A
-<v<A>>^A<vA<A>>^AAvAA<^A>A<v<A>>^AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A
+v<A<AA>>^A A v<<A>>^A A v<A^>A v<<A>>^A v<<A>>^A A (KO)
+
 """
 
-assert(Problem([]).find_complexity("029A")//29 == 68)
-assert(Problem([]).find_complexity("980A")//980 == 60)
-assert(Problem([]).find_complexity("179A")//179 == 68)
-assert(Problem([]).find_complexity("456A")//456 == 64)
-assert(Problem([]).find_complexity("379A")//379 == 64)
-# assert(Solver(in1).solve(1) == 126384)
-# assert(Solver(in1).solve(2) == 0)
-parts = [1]
+# print(Problem([]).fast_encode("<A^A>^^A", 1))
+# print(Problem([]).fast_encode("v<<A>>^A<A>AvA<^AA>A", 1))
+# print(Problem([]).fast_encode("<A^A>^^A", 2))
+# assert(Problem([]).real_complexity("029A", 2)//29 == 68)
+# assert(Problem([]).real_complexity("980A", 2)//980 == 60)
+# assert(Problem([]).real_complexity("179A", 2)//179 == 68)
+# assert(Problem([]).real_complexity("456A", 2)//456 == 64)
+# assert(Problem([]).real_complexity("379A", 2)//379 == 64)
+assert(Solver(in1).solve(1) == 126384)
+assert(Solver(".test").solve(1) == 205160)
+parts = [2]
+# 2157282750 in 12 steps
+
 
 ### No change after this ###
 
