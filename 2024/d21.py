347eeef3c9d35ef399543e608de177baf57b3013
Hai-Nam Nguyen <han.nguyen@criteo.com>
2024-12-22 07:38:39 +0100

    Day 21 Part 2

--- 2024/d21.py
+++ 2024/d21.py
@@ -56,7 +56,7 @@ class Problem:
                 continue
             if result and d > len(result[0]):
                 continue
-            trials = [2,3,0,1]
+            trials = [3,0,2,1]
             if seq:
                 # Favor the same key
                 prev = self.D.index(seq[-1])
@@ -68,19 +68,17 @@ class Problem:
         return max(result, key=self.score)
 
     @cache
-    def fast_shortest(self, A, B):
+    def ffs(self, A, B):
         return self.find_shortest(A, B, self.K2)
 
     def score(self, seq):
-        s = sum(1 for i in range(len(seq)-1) if seq[i]==seq[i+1])
-        # print(seq, s)
-        return s
+        return sum(1 for i in range(len(seq)-1) if seq[i]==seq[i+1])
 
     def encode(self, seq, keypad):
         res = []
         p = 'A'
         for s in seq:
-            res.append(self.find_shortest(p, s, keypad))
+            res.append(self.ffs(p, s) if len(keypad) < 10 else self.find_shortest(p, s, keypad))
             res.append('A')
             p = s
         return ''.join(res)
@@ -95,17 +93,10 @@ class Problem:
                 p = keypad[p][self.D.index(s)]
         return ''.join(res)
 
-    def find_complexity(self, seq):
-        s1 = self.encode(seq, self.K1)
-        # s1 = s1.replace('^^<<A', '<<^^A')
-        s2 = self.encode(s1, self.K2)
-        # s2 = s2.replace('^^<<A', '<<^^A')
-        s3 = self.encode(s2, self.K2)
-        # print(s1)
-        # print(s2)
-        # print(s3)
-        # print(seq, len(s3))
-        return int(seq[:-1]) * len(s3)
+    def debug(self, s3):
+        s2 = self.decode(s3, self.K2)
+        s1 = self.decode(s2, self.K2)
+        print(f"{s1}\n{s2}\n{s3}")
 
     @cache
     def fast_sum(self, seq, depth):
@@ -121,16 +112,7 @@ class Problem:
         s = self.fast_sum(seq2, depth)
         return int(seq[:-1]) * s
 
-    def debug(self, s3):
-        s2 = self.decode(s3, self.K2)
-        s1 = self.decode(s2, self.K2)
-        print(f"{s1}\n{s2}\n{s3}")
-
     def solve(self):
-        # print(self.find_complexity("179A"))
-        # print(self.find_shortest("A", "1", self.K1))
-        # self.debug("<v<A>>^A<vA<A>>^AAvAA<^A>A<v<A>>^AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A")
-        # return sum(self.find_complexity(seq) for seq in self.input)
         return sum(self.real_complexity(seq, 2) for seq in self.input)
 
     def solve2(self):
@@ -142,35 +124,10 @@ in1 = """
 179A
 456A
 379A
-
-v<<A>>^A<A>AvA<^AA>A<vAAA>^A
-<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A
-
-v<<A>>^A<A>AvA<^AA>Av<AAA^>A
-v<A<AA>>^AAv<<A>>^AAv<A^>Av<<A>>^Av<<A>>^AAv<A<A>>^Av<A<A>>^Av<A<A>>^AA
-
-<A^A>^^A
-v<<A>>^A<A>AvA<^AA>A
-v<A<AA>>^AvAA<^A>Av<<A>>^AvA^Av<A^>Av<<A>^A>AAvA^A
-
-
-v<A<AA>>^A A v<<A>>^A A v<A^>A v<<A>>^A v<<A>>^A A (KO)
-
 """
-
-# print(Problem([]).fast_encode("<A^A>^^A", 1))
-# print(Problem([]).fast_encode("v<<A>>^A<A>AvA<^AA>A", 1))
-# print(Problem([]).fast_encode("<A^A>^^A", 2))
-# assert(Problem([]).real_complexity("029A", 2)//29 == 68)
-# assert(Problem([]).real_complexity("980A", 2)//980 == 60)
-# assert(Problem([]).real_complexity("179A", 2)//179 == 68)
-# assert(Problem([]).real_complexity("456A", 2)//456 == 64)
-# assert(Problem([]).real_complexity("379A", 2)//379 == 64)
 assert(Solver(in1).solve(1) == 126384)
 assert(Solver(".test").solve(1) == 205160)
-parts = [2]
-# 2157282750 in 12 steps
-
+parts = [1, 2]
 
 ### No change after this ###
 
