d4139684e0b2378ee6b0981dc8fe6a76082fb978
Hai-Nam Nguyen <han.nguyen@criteo.com>
2025-12-01 09:32:40 +0100

    2025 preparation (a bit late, but...)

---
+++ 2025/template.py
@@ -0,0 +1,41 @@
+class Solver:
+    def __init__(self, input):
+        if input == '.test':
+            f = open(__file__[:-3] + input, 'r')
+            self.input = f.read().strip().split('\n')
+        else:
+            self.input = input.strip().split('\n')
+
+    def solve(self, part):
+        problem = Problem(self.input)
+        return problem.solve() if part==1 else problem.solve2()
+
+### No change before this ###
+
+import time
+
+
+class Problem:
+    def __init__(self, input):
+        self.input = input
+
+    def solve(self):
+        return 0
+
+    def solve2(self):
+        return 0
+
+in1 = """
+
+"""
+assert(Solver(in1).solve(1) == 0)
+# assert(Solver(in1).solve(2) == 0)
+parts = []
+
+### No change after this ###
+
+solver = Solver('.test')
+for part in parts:
+    start = time.time()
+    result = solver.solve(part)
+    print("Part %d: \033[1;31m%-10s\033[0m (%.0f ms)" % (part, result, (time.time() - start)*1000))
