fb59a86204a645bb9f4ec395b10785aeec44a06f
Hai-Nam Nguyen <hainam@jcisio.com>
2025-12-04 06:27:35 +0100

    Day 04 Part 2

--- 2025/d04.py
+++ 2025/d04.py
@@ -22,22 +22,37 @@ import time
 
 class Problem:
     def __init__(self, input):
-        self.input = input
+        self.input = [list(row) for row in input]
         self.x = len(input[0])
         self.y = len(input)
         self.adj = [(0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
 
     def count_adjacent(self, x, y):
-        return sum(1 for dx, dy in self.adj if self.is_accessible(x+dx, y+dy))
+        return sum(1 for dx, dy in self.adj if self.has_paper(x+dx, y+dy))
 
-    def is_accessible(self, x, y):
+    def has_paper(self, x, y):
         return 0 <= x < len(self.input[0]) and 0 <= y < len(self.input) and self.input[y][x] == '@'
 
+    def is_accessible(self, x, y):
+        return self.input[y][x] =='@' and self.count_adjacent(x, y)<4
+
     def solve(self):
-        return sum(1 for x in range(self.x) for y in range(self.y) if self.input[y][x] =='@' and self.count_adjacent(x, y)<4)
+        return sum(1 for x in range(self.x) for y in range(self.y) if self.is_accessible(x, y))
 
     def solve2(self):
-        return 0
+        to_check = set([(x, y) for x in range(self.x) for y in range(self.y) if self.input[y][x] == '@'])
+        s = 0
+        while True:
+            found_new = set()
+            for x, y in to_check:
+                if self.is_accessible(x, y):
+                    self.input[y][x] = '.'
+                    found_new.add((x, y))
+                    s += 1
+            if not found_new:
+                break
+            to_check -= found_new
+        return s
 
 in1 = """..@@.@@@@.
 @@@.@.@.@@
@@ -51,7 +66,7 @@ in1 = """..@@.@@@@.
 @.@.@@@.@.
 """
 assert(Solver(in1).solve(1) == 13)
-# assert(Solver(in1).solve(2) == 0)
+assert(Solver(in1).solve(2) == 43)
 parts = [1, 2]
 
 ### No change after this ###
